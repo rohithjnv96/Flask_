1cce8bd55138c82811fcbafaa190b4b75bded6c1
Rohith HB <rohithjnv96@gmail.com>
2023-12-01 19:04:14 +1100

    Implemented basic signup page

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask,render_template
+# refactor imports
+from flask import *
+from forms import SignUpForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SECRET_KEY'
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return render_template("home.html")
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit() and request.method == 'POST':
+        return "Almost logged in"
+    return render_template("signup_page.html", form=form)
+
+
+@app.route('/register_user', methods=['POST'])
+def register_user():
+    return f'{request.form["username"]} -> {request.form.get("email")}'
 
 
 if __name__ == '__main__':
---
+++ forms.py
@@ -0,0 +1,9 @@
+# rewrite below import
+from flask_wtf import *
+from wtforms import *
+from wtforms.validators import *
+
+
+class SignUpForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired(), Length(min=5, max=20)])
+    email = EmailField('email', validators=[DataRequired()])
