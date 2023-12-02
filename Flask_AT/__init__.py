c6ed989ff178c85eb719c2ba1d8a580d4430602e
Rohith HB <rohithjnv96@gmail.com>
2023-12-02 16:53:15 +1100

    Bug Fix: missing flask instance

--- Flask_AT/__init__.py
+++ Flask_AT/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
 
+
 def create_app():
     app = Flask(__name__)
+
     app.config['SECRET_KEY'] = 'SECRET_KEY'
+
+    from Flask_AT.main.app import main
+    app.register_blueprint(main)
+
     return app
--- Flask_AT/main/app.py
+++ Flask_AT/main/app.py
@@ -1,14 +1,16 @@
 # refactor imports
 from flask import *
-from forms import SignUpForm
+from Flask_AT.main.forms import SignUpForm
+
+main = Blueprint('main', __name__)
 
 
-@app.route('/')
+@main.route('/')
 def hello_world():
     return render_template("home.html")
 
 
-@app.route('/signup', methods=['GET', 'POST'])
+@main.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = SignUpForm()
     if form.validate_on_submit() and request.method == 'POST':
@@ -16,6 +18,6 @@ def signup():
     return render_template("signup_page.html", form=form)
 
 
-@app.route('/register_user', methods=['POST'])
+@main.route('/register_user', methods=['POST'])
 def register_user():
     return f'{request.form["username"]} -> {request.form.get("email")}'
